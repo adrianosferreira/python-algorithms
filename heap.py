3630703bb111562c3b76c9511338863c4be757cd
Adriano Ferreira <adriano.f@onthegosystems.com>
2020-04-30 16:00:52 -0300

    fixup

--- heap.py
+++ heap.py
@@ -27,12 +27,12 @@ class Heap:
         curr = 0
 
         while True:
-            if self.get_left_child(curr) < len(self.data) and self.data[curr] > self.data[self.get_left_child(curr)]:
+            if self.has_left_child(curr) and self.data[curr] > self.data[self.get_left_child(curr)]:
                 temp = self.data[curr]
                 self.data[curr] = self.data[self.get_left_child(curr)]
                 self.data[self.get_left_child(curr)] = temp
                 curr = self.get_left_child(curr)
-            elif self.get_right_child(curr) < len(self.data) and self.data[curr] > self.data[self.get_right_child(curr)]:
+            elif self.has_right_child(curr) < len(self.data) and self.data[curr] > self.data[self.get_right_child(curr)]:
                 temp = self.data[curr]
                 self.data[curr] = self.data[self.get_right_child(curr)]
                 self.data[self.get_right_child(curr)] = temp
@@ -61,7 +61,13 @@ class Heap:
         for x in data:
             self.insert(x)
 
+    def has_left_child(self, parent):
+        return self.get_left_child(parent) < len(self.data)
+
+    def has_right_child(self, parent):
+        return self.get_right_child(parent) < len(self.data)
+
 
 heap = Heap()
 heap.heapify([5, 1, 3, 10, 11, 15, 8])
-test = 1
+print(heap.data)
